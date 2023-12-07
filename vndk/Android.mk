99ed75829a1998370fef7481944f668faee23235
Debayan Kar <kardebayan3@gmail.com>
2023-12-07 19:56:45 +0530

    RM6785-common: vndk: Adapt for PixelOS

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -11,13 +11,3 @@ LOCAL_CHECK_ELF_FILES := false
 LOCAL_VENDOR_MODULE := true
 LOCAL_REQUIRED_MODULES := android.hardware.graphics.common-V2-ndk_platform
 include $(BUILD_PREBUILT)
-
-ifeq ($(LINEAGE_BUILD),)
-include $(CLEAR_VARS)
-LOCAL_MODULE := android.hardware.graphics.common-V2-ndk_platform
-LOCAL_SHARED_LIBRARIES := android.hardware.graphics.common-V2-ndk
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-endif
